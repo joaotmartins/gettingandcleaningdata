51f7a3b6c8f6540c565f44e667c0c75498d3618f
joaotmartins <joao.t.martins@gmail.com>
2015-11-22 16:47:51 +0000

    Added script and readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # gettingandcleaningdata
 
 This repository contains the code for the programming assignment of the Getting and Cleaning Data course.
+
+The code consists of a single script, called run_analysis.R. This script will assume the UCI HAR dataset has been 
+de-compressed to the current directory, and will produce a tidy summary of all average values of every variable in the original dataset. See the CodeBook.md file for a detailed description of the steps the script performs, and a detailed description of the produced output.
--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,52 @@
+# This script is designed to read in the entire UCI HAR dataset, and make a new,
+# cleaned up and smmarized data set out of it, outputting it to a file named
+# "tidy.csv"
 
-# Reads either the CUI HAR training or test set, and builds a data frame out of
+library(data.table)
+library(dplyr)
+
+
+# The columns to select from the original data set.
+interestingFeatCols <- 
+        c(1,2,3,4,5,6,41,42,43,44,45,46,81,82,83,84,85,86,121,122,123,124,
+          125,126,161,162,163,164,165,166,201,202,214,215,227,228,240,241,
+          253,254,266,267,268,269,270,271,345,346,347,348,349,350,424,425,
+          426,427,428,429,503,504,516,517,529,530,542,543,555,556,557,558,
+          559,560,561,562,563)
+
+# The new names for the resulting dataset's columns
+interestingFeatNames <-
+        c("tBodyAcc.mean.X","tBodyAcc.mean.Y","tBodyAcc.mean.Z",
+          "tBodyAcc.std.X","tBodyAcc.std.Y","tBodyAcc.std.Z",
+          "tGravityAcc.mean.X","tGravityAcc.mean.Y","tGravityAcc.mean.Z",
+          "tGravityAcc.std.X","tGravityAcc.std.Y","tGravityAcc.std.Z",
+          "tBodyAccJerk.mean.X","tBodyAccJerk.mean.Y","tBodyAccJerk.mean.Z",
+          "tBodyAccJerk.std.X","tBodyAccJerk.std.Y","tBodyAccJerk.std.Z",
+          "tBodyGyro.mean.X","tBodyGyro.mean.Y","tBodyGyro.mean.Z",
+          "tBodyGyro.std.X","tBodyGyro.std.Y","tBodyGyro.std.Z",
+          "tBodyGyroJerk.mean.X","tBodyGyroJerk.mean.Y","tBodyGyroJerk.mean.Z",
+          "tBodyGyroJerk.std.X","tBodyGyroJerk.std.Y","tBodyGyroJerk.std.Z",
+          "tBodyAccMag.mean","tBodyAccMag.std","tGravityAccMag.mean",
+          "tGravityAccMag.std","tBodyAccJerkMag.mean","tBodyAccJerkMag.std",
+          "tBodyGyroMag.mean","tBodyGyroMag.std","tBodyGyroJerkMag.mean",
+          "tBodyGyroJerkMag.std","fBodyAcc.mean.X","fBodyAcc.mean.Y",
+          "fBodyAcc.mean.Z","fBodyAcc.std.X","fBodyAcc.std.Y",
+          "fBodyAcc.std.Z","fBodyAccJerk.mean.X","fBodyAccJerk.mean.Y",
+          "fBodyAccJerk.mean.Z","fBodyAccJerk.std.X","fBodyAccJerk.std.Y",
+          "fBodyAccJerk.std.Z","fBodyGyro.mean.X","fBodyGyro.mean.Y",
+          "fBodyGyro.mean.Z","fBodyGyro.std.X","fBodyGyro.std.Y",
+          "fBodyGyro.std.Z","fBodyAccMag.mean","fBodyAccMag.std",
+          "fBodyBodyAccJerkMag.mean","fBodyBodyAccJerkMag.std",
+          "fBodyBodyGyroMag.mean","fBodyBodyGyroMag.std",
+          "fBodyBodyGyroJerkMag.mean","fBodyBodyGyroJerkMag.std",
+          "angle.tBodyAccMean.gravity","angle.tBodyAccJerkMean..gravityMean",
+          "angle.tBodyGyroMean.gravityMean",
+          "angle.tBodyGyroJerkMean.gravityMean","angle.X.gravityMean",
+          "angle.Y.gravityMean","angle.Z.gravityMean",
+          "activity", "subject")
+
+
+# Reads either the UCI HAR training or test set, and builds a data frame out of
 # it.
 #
 # Arguments:
@@ -21,7 +68,7 @@ readDataSet <- function(dataset, featureNames, activityLabels) {
         X <- fread(input = xfile, sep=" ", header = FALSE)
         
         # assign proper names to data
-        X <- setNames(X, featureNames$Name)
+        X <- setNames(X, make.names(featureNames$Name, unique=TRUE))
         
         #
         # transform activity data into a factor column
@@ -37,8 +84,8 @@ readDataSet <- function(dataset, featureNames, activityLabels) {
         actChars <- character(length(actNums))
         
         # fill in char column with activity names
-        for (n in actvlab$Index) {
-                actChars[actNums == activityLabels$Index[n]] <- actvlab$Label[n]
+        for (n in activityLabels$Index) {
+                actChars[actNums == activityLabels$Index[n]] <- activityLabels$Label[n]
         }
         
         # transform into factor
@@ -81,3 +128,14 @@ readAndCombineDataSet <- function() {
 }
 
 fs <- readAndCombineDataSet()
+
+# Select only interesting columns
+trimmedSet <- select(fs, interestingFeatCols)
+trimmedSet <- setNames(trimmedSet, interestingFeatNames)
+
+# summarize by taking the average of each variable by activity and subject
+summarizedSet <- group_by(trimmedSet, subject, activity) %>% 
+        summarise_each(funs(mean)) %>% arrange(subject)
+
+write.table(summarizedSet, file = "tidy_data.txt", row.names = FALSE)
+
